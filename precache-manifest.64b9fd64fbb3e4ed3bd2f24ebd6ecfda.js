399956e12df19e5ce0a0fb96c063eb321c1585b6
jeromemeichelbeck <jerome@meichelbeck.io>
2019-10-15 23:12:01 +0200

    Updates

---
+++ precache-manifest.64b9fd64fbb3e4ed3bd2f24ebd6ecfda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22aff4c3fb0aca55ba20657d57a04396",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a7b223f5b81e2bfd4638",
+    "url": "/monster-rolodex/static/css/main.55e4fd3a.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monster-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "a7b223f5b81e2bfd4638",
+    "url": "/monster-rolodex/static/js/main.a3cd8058.chunk.js"
+  },
+  {
+    "revision": "e1d991a2b9a3476b0dce",
+    "url": "/monster-rolodex/static/js/runtime-main.df1bd6b7.js"
+  }
+]);
